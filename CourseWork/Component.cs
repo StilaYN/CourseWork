070d183e2be369d53aa79190d9952b15416a59e5
StilaYN <dist.avdeev2004@gmail.com>
2023-11-17 18:45:21 +0700

    Для представления техники

--- CourseWork/Component.cs
+++ CourseWork/Component.cs
@@ -2,6 +2,25 @@
 
 public class Component
 {
+    public Component(string technicType, string componentType, string name, string article, int price)
+    {
+        _technicType = technicType;
+        _componentType = componentType;
+        _name = name;
+        _article = article;
+        _price = price;
+    }
+
+    public string TechnicType => _technicType;
+
+    public string ComponentType => _componentType;
+
+    public string Name => _name;
+
+    public string Article => _article;
+
+    public int Price => _price;
+
     private string _technicType;
     private string _componentType;
     private string _name;
