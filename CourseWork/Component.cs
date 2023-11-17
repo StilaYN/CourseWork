437915aa31956ce5642924893676c19070f5930d
StilaYN <dist.avdeev2004@gmail.com>
2023-11-17 18:36:02 +0700

    Для отображения детали

---
+++ CourseWork/Component.cs
@@ -0,0 +1,10 @@
+﻿namespace CourseWork;
+
+public class Component
+{
+    private string _technicType;
+    private string _componentType;
+    private string _name;
+    private string _article;
+    private int _price;
+}
