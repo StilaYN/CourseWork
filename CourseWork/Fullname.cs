adf62ff1d49fd5f4a736324b88427ea1c24a76d6
StilaYN <dist.avdeev2004@gmail.com>
2023-11-17 12:45:22 +0700

    Для удобного отображения имени фамилии отчества

--- CourseWork/Fullname.cs
+++ CourseWork/Fullname.cs
@@ -2,5 +2,29 @@
 
 public class Fullname
 {
-    
+    private string _firstName;//Имя
+    private string _middleName;//Отчество
+    private string _lastName;//Фамилия
+
+    public Fullname(string firstName, string middleName, string lastName)
+    {
+        _firstName = firstName;
+        _middleName = middleName;
+        _lastName = lastName;
+    }
+
+    public string FirstName => _firstName;
+
+    public string MiddleName => _middleName;
+
+    public string LastName => _lastName;
+
+    public override string ToString()
+    {
+        if (string.IsNullOrEmpty(_firstName) || string.IsNullOrEmpty(_lastName))
+        {
+            return base.ToString();
+        }
+        return _firstName + " " + _lastName + " " + _middleName;
+    }
 }
