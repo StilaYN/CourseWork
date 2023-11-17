71d5bf74d3540b5b5df7ff4d0ec2f66659a46830
StilaYN <dist.avdeev2004@gmail.com>
2023-11-17 12:54:57 +0700

    Для отображение информации о сотруднике

---
+++ CourseWork/Employee.cs
@@ -0,0 +1,12 @@
+﻿namespace CourseWork;
+
+public class Employee
+{
+    private Fullname _name;
+    public int _phoneNumber;
+    private string _email;
+    private string _position;
+    private string _login;
+    private string _password;
+
+}
