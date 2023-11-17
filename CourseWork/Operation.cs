fc3c1185c60efd56e0b29ca755cdabc1ffd2334a
StilaYN <dist.avdeev2004@gmail.com>
2023-11-17 19:06:58 +0700

    Для представления информации об совершенной операции и расчета итоговой цены

---
+++ CourseWork/Operation.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace CourseWork;
+
+public class Operation
+{
+    public Operation(string name, Component? usedComponent, int price)
+    {
+        _name = name;
+        _usedComponent = usedComponent;
+        _price = price;
+    }
+    public Operation(string name, int price)
+    {
+        _name = name;
+        _price = price;
+    }
+
+    public string Name
+    {
+        get => _name;
+        set => _name = value ?? throw new ArgumentNullException(nameof(value));
+    }
+
+    public Component? UsedComponent
+    {
+        get => _usedComponent;
+        set => _usedComponent = value;
+    }
+
+    public int Price
+    {
+        get => _price;
+        set => _price = value;
+    }
+    public int TotalPrice
+    {
+        get
+        {
+            if (_usedComponent != null) return _price + _usedComponent.Price;
+            else return _price;
+        }
+    }
+
+    private string _name;
+    private Component? _usedComponent;
+    private int _price;
+}
