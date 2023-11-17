5a1a91234432d9f658293bf30b53fb9c606efadd
StilaYN <dist.avdeev2004@gmail.com>
2023-11-17 14:59:35 +0700

    Для представления информации об работниках

--- CourseWork/Employee.cs
+++ CourseWork/Employee.cs
@@ -1,12 +1,62 @@
-﻿namespace CourseWork;
+﻿using System;
+
+namespace CourseWork;
 
 public class Employee
 {
-    private Fullname _name;
-    public int _phoneNumber;
+    public Employee(Fullname name, int phoneNumber, string email, string position, string login, string password)
+    {
+        _name = name;
+        _phoneNumber = phoneNumber;
+        _email = email;
+        _position = position;
+        _login = login;
+        _password = password;
+    }
+
+    public Employee(string firstName,string middleName, string lastName, int phoneNumber, string email, string position, string login, string password)
+    {
+        _name = new Fullname(firstName,middleName,lastName);
+        _phoneNumber = phoneNumber;
+        _email = email;
+        _position = position;
+        _login = login;
+        _password = password;
+    }
+
+    public int PhoneNumber
+    {
+        get => _phoneNumber;
+        set => _phoneNumber = value;
+    }
+
+    public string Email
+    {
+        get => _email;
+        set => _email = value ?? throw new ArgumentNullException(nameof(value));
+    }
+
+    public string Position
+    {
+        get => _position;
+        set => _position = value ?? throw new ArgumentNullException(nameof(value));
+    }
+
+    public string Password
+    {
+        get => _password;
+        set => _password = value ?? throw new ArgumentNullException(nameof(value));
+    }
+
+    public Fullname Name => _name;
+
+    public string Login => _login;
+
+    private readonly Fullname _name;
+    private int _phoneNumber;
     private string _email;
     private string _position;
-    private string _login;
+    private readonly string _login;
     private string _password;
 
 }
